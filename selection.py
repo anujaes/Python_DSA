0ac44d5006223bde22a647a531a8fbab4067e1a2
anujaes <anuj.aes@gmail.com>
2022-08-20 19:15:54 +0530

    [selection.py]: selection sort implemented

---
+++ selection.py
@@ -0,0 +1,24 @@
+from io import UnsupportedOperation
+
+
+def swap(list,pos1,pos2):
+    list[pos1],list[pos2] = list[pos2],list[pos1]
+    return list
+
+def Selection(arr):
+    list = arr[:]
+    length = len(list)
+    for i in range( 0,length):
+        smallest = i
+        for j in range(i+1,length):
+            if list[j] < list[smallest]:
+                smallest = j
+        swap(list,i,smallest)
+
+    return list
+
+unsorted = [12,4,122,6,4,78,0,2,4]
+sorted = Selection(unsorted)
+
+print('unsorted > ',unsorted)
+print('unsorted > ',sorted)
